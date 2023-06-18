d2e8766cd03be3892b5c85c14e9eead20167db0c
duongpham17 <duongpham17@icloud.com>
2023-06-18 18:32:43 +0100

    localhost

--- frontend/src/@redux/api.ts
+++ frontend/src/@redux/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = process.env.NODE_ENV === "production" ? process.env.REACT_APP_PRODUCTION_PORT_API : process.env.REACT_APP_DEVELOPMENT_PORT_API;
+const url = process.env.NODE_ENV === "production" ? "" : "http://localhost:8000";
 
 export const api = axios.create({
     baseURL: `${url}/api`,
