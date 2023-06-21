2b1d9000a6fcbc30ca8d7a263b050b44af5b15e8
duongpham17 <duongpham17@icloud.com>
2023-06-21 15:42:02 +0100

    removed build

--- build/@environment/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.development_frontend_url = exports.production_frontend_url = exports.website_url = exports.website_name = void 0;
-exports.website_name = "Trader";
-exports.website_url = [];
-exports.production_frontend_url = [];
-exports.development_frontend_url = ["http://localhost:3000"];
--- build/app/port.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const port = (app) => {
-    const port = process.env.PORT || 8000;
-    const development = process.env.NODE_ENV === "development";
-    const environment = () => development && console.log(`Listening on port ${port}`);
-    app.listen(port, environment);
-};
-exports.default = port;
